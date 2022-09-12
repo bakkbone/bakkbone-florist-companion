982b47100109dfcd549b385c03e53830c678f5b5
BAKKBONE Australia <hosting@bakkbone.com.au>
2022-09-12 19:04:43 +0800

    Update custom-posts.php

--- trunk/incl/custom-posts.php
+++ trunk/incl/custom-posts.php
@@ -36,19 +36,19 @@ class BkfCustomPosts{
 		
 		// labels for posts
 		$labels = array(
-			"name"					=> _x("Delivery Suburbs", "post type general name", "bakkbone-florist-companion"),
-			"singular_name"			=> _x("Delivery Suburb", "post type singular name", "bakkbone-florist-companion"),
-			"menu_name"				=> _x("Delivery Suburbs", "admin menu", "bakkbone-florist-companion"),
-			"name_admin_bar"		=> _x("Delivery Suburb", "add new on admin bar", "bakkbone-florist-companion"),
-			"add_new"				=> _x("Add New", "Add New", "bakkbone-florist-companion"),
-			"add_new_item"			=> __("Add New Suburb", "bakkbone-florist-companion"),
-			"new_item"				=> __("New Suburb", "bakkbone-florist-companion"),
-			"edit_item"				=> __("Edit Suburb", "bakkbone-florist-companion"),
-			"view_item"				=> __("View Suburb", "bakkbone-florist-companion"),
-			"all_items"				=> __("All Suburbs", "bakkbone-florist-companion"),
-			"search_items"			=> __("Search Suburbs", "bakkbone-florist-companion"),
-			"parent_item_colon"		=> __("Parent Suburbs:", "bakkbone-florist-companion"),
-			"not_found"				=> __("Not found", "bakkbone-florist-companion"),
+			"name"			=> _x("Delivery Suburbs", "post type general name", "bakkbone-florist-companion"),
+			"singular_name"		=> _x("Delivery Suburb", "post type singular name", "bakkbone-florist-companion"),
+			"menu_name"		=> _x("Delivery Suburbs", "admin menu", "bakkbone-florist-companion"),
+			"name_admin_bar"	=> _x("Delivery Suburb", "add new on admin bar", "bakkbone-florist-companion"),
+			"add_new"		=> _x("Add New", "Add New", "bakkbone-florist-companion"),
+			"add_new_item"		=> __("Add New Suburb", "bakkbone-florist-companion"),
+			"new_item"		=> __("New Suburb", "bakkbone-florist-companion"),
+			"edit_item"		=> __("Edit Suburb", "bakkbone-florist-companion"),
+			"view_item"		=> __("View Suburb", "bakkbone-florist-companion"),
+			"all_items"		=> __("All Suburbs", "bakkbone-florist-companion"),
+			"search_items"		=> __("Search Suburbs", "bakkbone-florist-companion"),
+			"parent_item_colon"	=> __("Parent Suburbs:", "bakkbone-florist-companion"),
+			"not_found"		=> __("Not found", "bakkbone-florist-companion"),
 			"not_found_in_trash"	=> __("Not found in trash", "bakkbone-florist-companion")
 		);
 		
