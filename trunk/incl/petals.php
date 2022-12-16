a2926b81951b6d2dfd0b37292b3c0e2d928ea612
BAKKBONE Australia <hosting@bakkbone.com.au>
2022-12-16 10:39:18 +0800

    Create petals.php

---
+++ trunk/incl/petals.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @author BAKKBONE Australia
+ * @package BkfPetals
+ * @license GNU General Public License (GPL) 3.0
+**/
+
+defined("BKF_EXEC") or die("Silence is golden");
+
+/**
+ * BkfPetals
+**/
+class BkfPetals{
+  
+  function __construct() {
+    
+  }
+  
+  
+  
+}
