684aab00a36f07f7b07db3face8691c1ca074ba4
BAKKBONE Australia <hosting@bakkbone.com.au>
2022-12-24 17:23:22 +0800

    add pdf class file

---
+++ trunk/incl/pdf/pdf.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author BAKKBONE Australia
+ * @package BkfPdf
+ * @license GNU General Public License (GPL) 3.0
+**/
+
+defined("BKF_EXEC") or die("Silence is golden");
+
+/**
+ * BkfPetals
+**/
+class BkfPdf{
+  
+}
