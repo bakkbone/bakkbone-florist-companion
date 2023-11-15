ce833383585d72397afe047defaeb327e486c6ca
BAKKBONE Australia <hosting@bakkbone.com.au>
2023-11-15 10:43:46 +0800

    3.5.1

---
+++ tags/3.5.1/src/dd/calendar.php
@@ -0,0 +1,288 @@
+<?php
+/**
+ * @author BAKKBONE Australia
+ * @package BKF_Delivery_Date_Calendar
+ * @license GNU General Public License (GPL) 3.0
+**/
+
+defined("BKF_EXEC") or die("Ah, sweet silence.");
+
+class BKF_Delivery_Date_Calendar{
+	
+	function __construct(){
+		add_action("admin_menu", [$this, "page"], 3);
+	}
+
+	function page(){
+		$admin_page = add_menu_page(
+			__("Delivery Calendar","bakkbone-florist-companion"),
+			__("Delivery Calendar","bakkbone-florist-companion"),
+			"manage_woocommerce",
+			"bkf_dc",
+			[$this, "bkfCalendarPageContent"],
+			'dashicons-calendar-alt',
+			2.4
+		);
+		add_action( 'load-'.$admin_page, [$this, 'bkf_cal_help_tab'] );
+	}
+	
+	function bkf_cal_help_tab(){
+		$screen = get_current_screen();
+		$id = 'bkf_cal_help';
+		$callback = [$this, 'bkf_cal_help'];
+		$screen->add_help_tab( array( 
+		   'id' => $id,
+		   'title' => __('Documentation','bakkbone-florist-companion'),
+		   'callback' => $callback
+		) );	
+	}
+	
+	function bkf_cal_help(){
+		?>
+		<h2><?php esc_html_e('View documentation for this page at: ','bakkbone-florist-companion'); ?></h2>
+			<a href="https://docs.floristpress.org/day-to-day/delivery-calendar/" target="_blank">https://docs.floristpress.org/day-to-day/delivery-calendar/</a>
+		<?php
+	}
+	
+	function bkfCalendarPageContent(){
+	    $startyear = wp_date("Y");
+	    $startmonth = wp_date("m") - 3;
+	    $startmonth = substr("0{$startmonth}", -2);
+	    $startday = "01";
+	    $startdate = $startyear."-".$startmonth."-".$startday;
+		?>
+		<div class="wrap">
+			<div class="bkf-box">
+			<h1><?php  esc_html_e("Delivery Calendar","bakkbone-florist-companion") ?></h1>
+				<div class="inside">
+	<script>
+	  document.addEventListener('DOMContentLoaded', function() {
+		var calendarEl = document.getElementById('calendar');
+		var calendar = new FullCalendar.Calendar(calendarEl, {
+			timeZone: "<?php echo wp_timezone_string(); ?>",
+			initialView: 'dayGridMonth',
+			navLinks: true,
+			validRange: {
+			    start: "<?php echo $startdate; ?>"
+			},
+			views: {
+				dayGridMonth: {
+					dayMaxEventRows: 4,
+					dayHeaderFormat: {
+						weekday: 'long'
+					}
+				},
+				dayGridWeek: {
+					dayHeaderFormat: {
+						weekday: 'long',
+						day: 'numeric',
+						month: 'numeric',
+						omitCommas: true
+					}
+				}
+			},
+			customButtons: {
+			   CSV: {
+				 text: '<?php esc_html_e("CSV", "bakkbone-florist-companion"); ?>',
+				 click: function() {
+					 var startYear = calendar.view.currentStart.getFullYear();
+					 var startMonth = calendar.view.currentStart.getMonth() + 1;
+					 var startDate = calendar.view.currentStart.getDate();
+					 var endYear = calendar.view.currentEnd.getFullYear();
+					 var endMonth = calendar.view.currentEnd.getMonth() + 1;
+					 var endDate = calendar.view.currentEnd.getDate();
+					 window.location.href = '<?php echo admin_url('admin-ajax.php?action=bkf_cal_csv&starty='); ?>' + startYear + '&startm=' + startMonth + '&startd=' + startDate + '&endy=' + endYear + '&endm=' + endMonth + '&endd=' + endDate;
+				 }
+			   },
+			   PDF: {
+				 text: '<?php esc_html_e("PDF", "bakkbone-florist-companion"); ?>',
+				 click: function() {
+					 var startYear = calendar.view.currentStart.getFullYear();
+					 var startMonth = calendar.view.currentStart.getMonth() + 1;
+					 var startDate = calendar.view.currentStart.getDate();
+					 var endYear = calendar.view.currentEnd.getFullYear();
+					 var endMonth = calendar.view.currentEnd.getMonth() + 1;
+					 var endDate = calendar.view.currentEnd.getDate();
+					 window.location.href = '<?php echo admin_url('admin-ajax.php?action=bkf_cal_pdf&starty='); ?>' + startYear + '&startm=' + startMonth + '&startd=' + startDate + '&endy=' + endYear + '&endm=' + endMonth + '&endd=' + endDate;
+				 }
+			   }
+			   
+			 },
+			headerToolbar: {
+				start: 'dayGridMonth,listMonth dayGridWeek,listWeek dayGridDay,listDay',
+				center: 'title',
+				end: 'CSV,PDF today prev,next'
+			},
+			footerToolbar: {
+				start: 'dayGridMonth,listMonth dayGridWeek,listWeek dayGridDay,listDay',
+				center: 'CSV,PDF',
+				end: 'today prev,next'
+			},
+			eventClick: function(info) {
+				info.jsEvent.preventDefault();
+				if (jQuery('#bkf_fullcalendar_modal').length) {
+					jQuery('#bkf_fullcalendar_modal').remove();
+				}
+				if (info.event.url.length) {
+					var modalContent = '<div id="bkf_fullcalendar_modal" title="' + info.event.title + '">' + info.event.extendedProps.content + '</div>';
+					jQuery('body').append(modalContent);
+					jQuery('#bkf_fullcalendar_modal').dialog({
+						modal: true,
+						minWidth: 350,
+						draggable: false,
+						resizable: false,
+						position: {
+							my: 'left top',
+							at: 'left bottom',
+							of: info.el
+						},
+						buttons: [
+							{
+								text: "<?php echo esc_html(sprintf(__('Print %s','bakkbone-florist-companion'),get_option('bkf_pdf_setting')['ws_title'])); ?>",
+								icon: "ui-icon-print",
+								click: function(){window.location.href = info.event.extendedProps.wsurl;}
+							},
+							{
+								text: "<?php esc_html_e('View Order','bakkbone-florist-companion'); ?>",
+								icon: "ui-icon-extlink",
+								click: function(){window.open(info.event.url)}
+							},
+						]
+					});
+				}
+			},
+			firstDay: 1,
+			height: '80vh',
+			events:[
+				<?php
+		$oldest = strtotime($startdate);
+		$orders = wc_get_orders(array(
+			'status'   => array('wc-new','wc-accept','wc-processing','wc-completed','wc-scheduled','wc-prepared','wc-collect','wc-out','wc-relayed','wc-invoiced'),
+			'limit'   => '-1',
+			'meta_key'      => '_delivery_timestamp',
+			'meta_value'    => $oldest,
+			'meta_compare'  => '>=',
+		));
+		foreach($orders as $k => $o){
+		    if($o->get_meta('_delivery_timestamp',true) < $oldest){
+		        unset($orders[$k]);
+		    }
+		}
+		$closed = get_option('bkf_dd_closed');
+		$full = get_option('bkf_dd_full');
+		$ct = __("Closed", "bakkbone-florist-companion");
+		$gt = __("Closed (Global)", "bakkbone-florist-companion");
+		$ft = __("Fully Booked", "bakkbone-florist-companion");
+		
+		foreach($orders as $order){
+			$id = $order->get_id();
+			if($order->get_meta('_delivery_timestamp',true) !== ''){
+				$url = $order->get_edit_order_url();
+				$ws = admin_url('admin-ajax.php?action=bkfdw&order_id='.$id.'&nonce='.wp_create_nonce('bkf'));
+				$date = wp_date("Y-m-d",$order->get_meta('_delivery_timestamp',true));
+				$recipient = str_replace('"', '\"', $order->get_meta('_shipping_first_name',true).' '.$order->get_meta('_shipping_last_name',true));
+				$customer = str_replace('"', '\"', $order->get_meta('_billing_first_name',true).' '.$order->get_meta('_billing_last_name',true));
+				$suburb = str_replace('"', '\"', $order->get_meta('_shipping_city',true));
+				if($order->needs_shipping_address()){
+					$address = $order->get_formatted_shipping_address().'<br/>'.esc_html($order->get_shipping_phone()).'<br/>'.esc_html($order->get_meta('_shipping_notes', true));
+					if($order->get_meta('_delivery_timeslot_id',true) !== ''){
+						$allts = bkf_get_timeslots_associative();
+						$tsid = 'ts'.$order->get_meta('_delivery_timeslot_id',true);
+						$ts = $allts[$tsid];
+						if($ts){
+							$start = $ts['start'];
+							$end = $ts['end'];
+							echo str_replace(["\r","\n"],"<br/>",'{ title: "#'.$order->ID.' - '. $recipient . ', ' . $suburb . '", url: "'.html_entity_decode($url).'", start: "'.$date.' '.$start.':00", end: "'.$date.' '.$end.':00", classNames: ["shipped"], wsurl: "'.$ws.'", content: "'.bkf_get_timeslot_string($order->get_meta('_delivery_timeslot_id',true)).'<br/>'.$address.'" }, ');
+						} else {
+							$timeslot = $order->get_meta('_delivery_timeslot',true);
+							$array = explode(" - ",$timeslot);
+							$start = date("H:i",strtotime($array[0]));
+							$end = date("H:i",strtotime($array[1]));
+							echo str_replace(["\r","\n"],"<br/>",'{ title: "#'.$order->ID.' - '. $recipient . ', ' . $suburb . '", url: "'.html_entity_decode($url).'", start: "'.$date.' '.$start.':00", end: "'.$date.' '.$end.':00", classNames: ["shipped"], wsurl: "'.$ws.'", content: "'.$timeslot.'<br/>'.$address.'" }, ');
+						}
+					} elseif($order->get_meta('_delivery_timeslot',true) !== '') {
+						$timeslot = $order->get_meta('_delivery_timeslot',true);
+						$array = explode(" - ",$timeslot);
+						$start = date("H:i",strtotime($array[0]));
+						$end = date("H:i",strtotime($array[1]));
+						echo str_replace(["\r","\n"],"<br/>",'{ title: "#'.$order->ID.' - '. $recipient . ', ' . $suburb . '", url: "'.html_entity_decode($url).'", start: "'.$date.' '.$start.':00", end: "'.$date.' '.$end.':00", classNames: ["shipped"], wsurl: "'.$ws.'", content: "'.$timeslot.'<br/>'.$address.'" }, ');
+					} else {
+						echo str_replace(["\r","\n"],"<br/>",'{ title: "#'.$order->ID.' - '. $recipient . ', ' . $suburb . '", url: "'.html_entity_decode($url).'", start: "'.$date.'", classNames: ["shipped"], wsurl: "'.$ws.'", content: "'.$address.'" }, ');
+					}
+				} else {
+					if($order->get_meta('_delivery_timeslot_id',true) !== ''){
+						$allts = bkf_get_timeslots_associative();
+						$tsid = 'ts'.$order->get_meta('_delivery_timeslot_id',true);
+						$ts = $allts[$tsid];
+						if($ts){
+							$start = $ts['start'];
+							$end = $ts['end'];
+							echo str_replace(["\r","\n"],"<br/>",'{ title: "#'.$order->ID.' - '.esc_html__('Pickup', 'bakkbone-florist-companion').', '.$customer.'", url: "'.html_entity_decode($url).'", start: "'.$date.' '.$start.':00", end: "'.$date.' '.$end.':00", classNames: ["unshipped"], wsurl: "'.$ws.'", content: "'.bkf_get_timeslot_string($order->get_meta('_delivery_timeslot_id',true)).'" }, ');
+						} else {
+							$timeslot = $order->get_meta('_delivery_timeslot',true);
+							$array = explode(" - ",$timeslot);
+							$start = date("H:i",strtotime($array[0]));
+							$end = date("H:i",strtotime($array[1]));
+							echo str_replace(["\r","\n"],"<br/>",'{ title: "#'.$order->ID.' - '.esc_html__('Pickup', 'bakkbone-florist-companion').', '.$customer.'", url: "'.html_entity_decode($url).'", start: "'.$date.' '.$start.':00", end: "'.$date.' '.$end.':00", classNames: ["unshipped"], wsurl: "'.$ws.'", content: "'.$timeslot.'" }, ');
+						}
+					} elseif($order->get_meta('_delivery_timeslot',true) !== '') {
+						$timeslot = $order->get_meta('_delivery_timeslot',true);
+						$array = explode(" - ",$timeslot);
+						$start = date("H:i",strtotime($array[0]));
+						$end = date("H:i",strtotime($array[1]));
+						echo str_replace(["\r","\n"],"<br/>",'{ title: "#'.$order->ID.' - '.esc_html__('Pickup', 'bakkbone-florist-companion').', '.$customer.'", url: "'.html_entity_decode($url).'", start: "'.$date.' '.$start.':00", end: "'.$date.' '.$end.':00", classNames: ["unshipped"], wsurl: "'.$ws.'", content: "'.$timeslot.'" }, ');
+					} else {
+						echo str_replace(["\r","\n"],"<br/>",'{ title: "#'.$order->ID.' - '.esc_html__('Pickup', 'bakkbone-florist-companion').', '.$customer.'", url: "'.html_entity_decode($url).'", start: "'.$date.'", classNames: ["unshipped"], wsurl: "'.$ws.'", content: "'.$order->get_meta('_delivery_date',true).'" }, ');
+					}
+				}
+				
+			}
+		}
+		if(null !== $closed){
+			foreach($closed as $ts => $date){
+				$string = wp_date("Y-m-d",$ts);
+				echo '{ title: \''.$ct.'\', start: \'' . $string . '\', display: \'background\', className: \'closedbg\' }, ';
+			}
+		}
+		if(null !== $full){
+			foreach($full as $ts => $date){
+				$string = wp_date("Y-m-d",$ts);
+				echo '{ title: \''.$ft.'\', start: \''.$string.'\', display: \'background\', className: \'fullbg\' }, ';
+			}
+		}
+		if(get_option('bkf_dd_setting')['monday'] == false){
+			echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'1\', display: \'background\', className: \'closedbg\' }, ';
+		}
+		if(get_option('bkf_dd_setting')['tuesday'] == false){
+			echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'2\', display: \'background\', className: \'closedbg\' }, ';
+		}
+		if(get_option('bkf_dd_setting')['wednesday'] == false){
+			echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'3\', display: \'background\', className: \'closedbg\' }, ';
+		}
+		if(get_option('bkf_dd_setting')['thursday'] == false){
+			echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'4\', display: \'background\', className: \'closedbg\' }, ';
+		}
+		if(get_option('bkf_dd_setting')['friday'] == false){
+			echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'5\', display: \'background\', className: \'closedbg\' }, ';
+		}
+		if(get_option('bkf_dd_setting')['saturday'] == false){
+			echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'6\', display: \'background\', className: \'closedbg\' }, ';
+		}
+		if(get_option('bkf_dd_setting')['sunday'] == false){
+			echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'0\', display: \'background\', className: \'closedbg\' }, ';
+		}
+		?>
+		]
+		});
+		calendar.render();
+	  });
+
+	</script>
+	  <div id='calendar'></div>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+	
+}
---
+++ tags/3.5.1/src/suburbs/method.php
@@ -0,0 +1,272 @@
+<?php
+/**
+ * @author BAKKBONE Australia
+ * @package BKF_Suburbs_Shipping_Method
+ * @license GNU General Public License (GPL) 3.0
+**/
+
+defined("BKF_EXEC") or die("Ah, sweet silence.");
+
+function BKF_Suburbs_filter_shipping_methods($rates, $package){
+	$sm = bkf_get_shipping_rates();	
+	$customer_sub = strtoupper( isset( $_REQUEST['s_city'] ) ? $_REQUEST['s_city'] : ( isset ( $_REQUEST['calc_shipping_city'] ) ? $_REQUEST['calc_shipping_city'] : ( ! empty( $user_city = WC()->customer->get_shipping_city() ) ? $user_city : WC()->countries->get_base_city() ) ) );
+	
+	foreach($sm as $smethod){
+		if($smethod['type'] == 'floristpress' && !empty($smethod['method_suburbs'])){
+			$suburbs = array_map('strtoupper', $smethod['method_suburbs']);
+			
+			if(!in_array($customer_sub, $suburbs)){
+				unset($rates[$smethod['rateid']]);
+			}
+		} elseif($smethod['type'] == 'floristpress' && empty($smethod['method_suburbs'])) {
+			unset($rates[$smethod['rateid']]);
+		}
+	}
+	return $rates;
+}
+
+add_filter('woocommerce_package_rates', 'BKF_Suburbs_filter_shipping_methods', PHP_INT_MAX - 1, 2);
+
+function BKF_Suburbs_add_shipping_method( $methods ) {
+	$methods['floristpress'] = 'BKF_Shipping_Method';
+	return $methods;
+}
+
+add_filter( 'woocommerce_shipping_methods', 'BKF_Suburbs_add_shipping_method' );
+
+function BKF_Suburbs_init_shipping_method(){
+	if ( ! class_exists( 'BKF_Shipping_Method' ) ) {
+		class BKF_Shipping_Method extends WC_Shipping_Method {
+			protected $fee_cost = '';
+
+			public $cost;
+
+			public $type;
+
+			public function __construct( $instance_id = 0 ) {
+				
+				$this->id                 = 'floristpress';
+				$this->instance_id        = absint( $instance_id );
+				$this->method_order       = 1;
+				$this->method_title       = __( 'FloristPress Suburbs List', 'bakkbone-florist-companion' );
+				$this->method_description = __( 'Uses the FloristPress Delivery Suburbs feature to restrict the method per suburb within the Zone.', 'bakkbone-florist-companion' );
+				$this->supports           = array(
+					'shipping-zones',
+					'instance-settings',
+					'instance-settings-modal',
+				);
+				$this->init();
+
+				add_action( 'woocommerce_update_options_shipping_' . $this->id, [$this, 'process_admin_options'] );
+			}
+
+			public function init() {
+				$this->instance_form_fields = include __DIR__ . '/method-settings.php';
+				$this->title                = $this->get_option( 'title' );
+				$this->tax_status           = $this->get_option( 'tax_status' );
+				$this->cost                 = $this->get_option( 'cost' );
+				$this->type                 = $this->get_option( 'type', 'class' );
+			}
+
+			protected function evaluate_cost( $sum, $args = [] ) {
+				// Add warning for subclasses.
+				if ( ! is_array( $args ) || ! array_key_exists( 'qty', $args ) || ! array_key_exists( 'cost', $args ) ) {
+					wc_doing_it_wrong( __FUNCTION__, '$args must contain `cost` and `qty` keys.', '4.0.1' );
+				}
+
+				include_once WC()->plugin_path() . '/includes/libraries/class-wc-eval-math.php';
+
+				// Allow 3rd parties to process shipping cost arguments.
+				$args           = apply_filters( 'woocommerce_evaluate_shipping_cost_args', $args, $sum, $this );
+				$locale         = localeconv();
+				$decimals       = array( wc_get_price_decimal_separator(), $locale['decimal_point'], $locale['mon_decimal_point'], ',' );
+				$this->fee_cost = $args['cost'];
+
+				// Expand shortcodes.
+				add_shortcode( 'fee', [$this, 'fee'] );
+
+				$sum = do_shortcode(
+					str_replace(
+						array(
+							'[qty]',
+							'[cost]',
+						),
+						array(
+							$args['qty'],
+							$args['cost'],
+						),
+						$sum
+					)
+				);
+
+				remove_shortcode( 'fee', [$this, 'fee'] );
+
+				// Remove whitespace from string.
+				$sum = preg_replace( '/\s+/', '', $sum );
+
+				// Remove locale from string.
+				$sum = str_replace( $decimals, '.', $sum );
+
+				// Trim invalid start/end characters.
+				$sum = rtrim( ltrim( $sum, "\t\n\r\0\x0B+*/" ), "\t\n\r\0\x0B+-*/" );
+
+				// Do the math.
+				return $sum ? WC_Eval_Math::evaluate( $sum ) : 0;
+			}
+
+			public function fee( $atts ) {
+				$atts = shortcode_atts(
+					array(
+						'percent' => '',
+						'min_fee' => '',
+						'max_fee' => '',
+					),
+					$atts,
+					'fee'
+				);
+
+				$calculated_fee = 0;
+
+				if ( $atts['percent'] ) {
+					$calculated_fee = $this->fee_cost * ( floatval( $atts['percent'] ) / 100 );
+				}
+
+				if ( $atts['min_fee'] && $calculated_fee < $atts['min_fee'] ) {
+					$calculated_fee = $atts['min_fee'];
+				}
+
+				if ( $atts['max_fee'] && $calculated_fee > $atts['max_fee'] ) {
+					$calculated_fee = $atts['max_fee'];
+				}
+
+				return $calculated_fee;
+			}
+
+			public function calculate_shipping( $package = [] ) {
+				$rate = array(
+					'id'      => $this->get_rate_id(),
+					'label'   => $this->title,
+					'cost'    => 0,
+					'package' => $package,
+				);
+
+				// Calculate the costs.
+				$has_costs = false; // True when a cost is set. False if all costs are blank strings.
+				$cost      = $this->get_option( 'cost' );
+
+				if ( '' !== $cost ) {
+					$has_costs    = true;
+					$rate['cost'] = $this->evaluate_cost(
+						$cost,
+						array(
+							'qty'  => $this->get_package_item_qty( $package ),
+							'cost' => $package['contents_cost'],
+						)
+					);
+				}
+
+				// Add shipping class costs.
+				$shipping_classes = WC()->shipping()->get_shipping_classes();
+
+				if ( ! empty( $shipping_classes ) ) {
+					$found_shipping_classes = $this->find_shipping_classes( $package );
+					$highest_class_cost     = 0;
+
+					foreach ( $found_shipping_classes as $shipping_class => $products ) {
+						// Also handles BW compatibility when slugs were used instead of ids.
+						$shipping_class_term = get_term_by( 'slug', $shipping_class, 'product_shipping_class' );
+						$class_cost_string   = $shipping_class_term && $shipping_class_term->term_id ? $this->get_option( 'class_cost_' . $shipping_class_term->term_id, $this->get_option( 'class_cost_' . $shipping_class, '' ) ) : $this->get_option( 'no_class_cost', '' );
+
+						if ( '' === $class_cost_string ) {
+							continue;
+						}
+
+						$has_costs  = true;
+						$class_cost = $this->evaluate_cost(
+							$class_cost_string,
+							array(
+								'qty'  => array_sum( wp_list_pluck( $products, 'quantity' ) ),
+								'cost' => array_sum( wp_list_pluck( $products, 'line_total' ) ),
+							)
+						);
+
+						if ( 'class' === $this->type ) {
+							$rate['cost'] += $class_cost;
+						} else {
+							$highest_class_cost = $class_cost > $highest_class_cost ? $class_cost : $highest_class_cost;
+						}
+					}
+
+					if ( 'order' === $this->type && $highest_class_cost ) {
+						$rate['cost'] += $highest_class_cost;
+					}
+				}
+
+				if ( $has_costs ) {
+					$this->add_rate( $rate );
+				}
+
+				do_action( 'woocommerce_' . $this->id . '_shipping_add_rate', $this, $rate );
+			}
+
+			public function get_package_item_qty( $package ) {
+				$total_quantity = 0;
+				foreach ( $package['contents'] as $item_id => $values ) {
+					if ( $values['quantity'] > 0 && $values['data']->needs_shipping() ) {
+						$total_quantity += $values['quantity'];
+					}
+				}
+				return $total_quantity;
+			}
+
+			public function find_shipping_classes( $package ) {
+				$found_shipping_classes = [];
+
+				foreach ( $package['contents'] as $item_id => $values ) {
+					if ( $values['data']->needs_shipping() ) {
+						$found_class = $values['data']->get_shipping_class();
+
+						if ( ! isset( $found_shipping_classes[ $found_class ] ) ) {
+							$found_shipping_classes[ $found_class ] = [];
+						}
+
+						$found_shipping_classes[ $found_class ][ $item_id ] = $values;
+					}
+				}
+
+				return $found_shipping_classes;
+			}
+
+			public function sanitize_cost( $value ) {
+				$value = is_null( $value ) ? '' : $value;
+				$value = wp_kses_post( trim( wp_unslash( $value ) ) );
+				$value = str_replace( array( get_woocommerce_currency_symbol(), html_entity_decode( get_woocommerce_currency_symbol() ) ), '', $value );
+				// Thrown an error on the front end if the evaluate_cost will fail.
+				$dummy_cost = $this->evaluate_cost(
+					$value,
+					array(
+						'cost' => 1,
+						'qty'  => 1,
+					)
+				);
+				if ( false === $dummy_cost ) {
+					throw new Exception( WC_Eval_Math::$last_error );
+				}
+				return $value;
+			}
+			
+			public function sanitize_suburbs( $value ) {
+				$array = explode("\r\n",$value);
+				sort($array);
+				$newarray = [];
+				foreach($array as $item){
+					$newarray[] = sanitize_text_field($item);
+				}
+				$newvalue = implode("\r\n",$newarray);
+				return $newvalue;
+			}
+		}
+	}
+}
+
+add_action('woocommerce_shipping_init', 'BKF_Suburbs_init_shipping_method');
