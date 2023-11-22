c8cb0199c9652f70847c9461e76ccda655713060
BAKKBONE Australia <hosting@bakkbone.com.au>
2023-11-22 17:49:10 +0800

    4.0.2

---
+++ tags/4.0.2/src/dd/blocks.php
@@ -0,0 +1,313 @@
+<?php
+/**
+ * @author BAKKBONE Australia
+ * @package BKF_Delivery_Date_Block
+ * @license GNU General Public License (GPL) 3.0
+**/
+
+defined("__BKF_EXEC__") or die("Ah, sweet silence.");
+
+class BKF_Delivery_Date_Block{
+
+	private $bkf_dd_closed = [];
+	private $bkf_dd_full = [];
+
+	function __construct(){
+		$this->bkf_dd_closed = get_option("bkf_dd_closed");
+		$this->bkf_dd_full = get_option("bkf_dd_full");
+		add_action("init", [$this, 'bkfScheduleDdPurge']);
+		add_action("bkf_dd_purge", [$this, 'bkfDdPurge']);
+		add_action("admin_menu", [$this,"bkf_admin_menu"], 5);
+		add_action("admin_init", [$this,"bkfAddDdBlocksInit"]);
+	}
+	
+	function bkfScheduleDdPurge() {
+		if ( false === as_has_scheduled_action( 'bkf_dd_purge' ) ){
+			as_schedule_recurring_action( strtotime( 'tomorrow' ), DAY_IN_SECONDS, 'bkf_dd_purge', [], '', true );
+		}
+	}
+	
+	function bkfDdPurge() {
+		$closed = get_option('bkf_dd_closed');
+		$full = get_option('bkf_dd_full');
+		$datefees = get_option('bkf_dd_ds_fees');
+		foreach($closed as $ts => $date){
+			if($ts < (string)strtotime(wp_date("Y-m-d"))){
+				unset($closed[$ts]);
+				update_option('bkf_dd_closed', $closed);
+			}
+		}
+		foreach($full as $ts => $date){
+			if($ts < (string)strtotime(wp_date("Y-m-d"))){
+				unset($full[$ts]);
+				update_option('bkf_dd_full', $full);
+			}
+		}
+		foreach($datefees as $ts => $fee){
+			if($ts < (string)strtotime(wp_date("Y-m-d"))){
+				unset($datefees[$ts]);
+				update_option('bkf_dd_ds_fees', $datefees);
+			}
+		}
+	}
+
+	function bkf_admin_menu(){
+		$admin_page = add_submenu_page(
+		"bkf_dd",
+		__("Blocked Delivery Dates","bakkbone-florist-companion"),
+		__("Blocked Dates","bakkbone-florist-companion"),
+		"manage_woocommerce",
+		"bkf_ddb",
+		[$this, 'bkf_dd_blocks_page'],
+		5
+		);
+		add_action( 'load-'.$admin_page, [$this, 'bkf_ddb_help_tab'] );
+	}
+	
+	function bkf_ddb_help_tab(){
+		$screen = get_current_screen();
+		$id = 'bkf_ddb_help';
+		$callback = [$this, 'bkf_ddb_help'];
+		$screen->add_help_tab( array( 
+		   'id' => $id,
+		   'title' => __('Documentation','bakkbone-florist-companion'),
+		   'callback' => $callback
+		) );	
+	}
+	
+	function bkf_ddb_help(){
+		?>
+		<h2><?php esc_html_e('View documentation for this page at: ','bakkbone-florist-companion'); ?></h2>
+			<a href="https://docs.floristpress.org/dd/blocked-dates/" target="_blank">https://docs.floristpress.org/dd/blocked-dates/</a>
+		<?php
+	}
+	
+	function bkf_dd_blocks_page(){
+		$bkf_dd_closed = get_option("bkf_dd_closed");
+		$closedsort = $bkf_dd_closed;
+		ksort($closedsort);
+		$bkf_dd_full = get_option("bkf_dd_full");
+		$fullsort = $bkf_dd_full;
+		ksort($fullsort);
+		$nonce = wp_create_nonce("bkf");
+		$ajaxurl = admin_url('admin-ajax.php');
+		$phtext = __("Date", "bakkbone-florist-companion");
+		$ubtext = __("Unblock Date", "bakkbone-florist-companion");
+		?>
+
+		<div class="wrap">
+			<div class="bkf-box">
+			<h1><?php esc_html_e("Delivery Date Blocks","bakkbone-florist-companion") ?></h1>
+			<p style="margin-top:0;"><?php esc_html_e("Click a block on the calendar to delete it.", "bakkbone-florist-companion"); ?></p>
+			<div style="display:flex;">
+				<div class="bkf-form" style="max-width:200px;text-align:center;margin:0 5px 5px 0;">
+					<form id="add-closed" action="<?php echo $ajaxurl; ?>" />
+						<h4 style="margin:0;"><?php esc_html_e('Add Closure Date', 'bakkbone-florist-companion'); ?></h4>
+						<?php wp_nonce_field('bkf', 'nonce'); ?>
+						<input type="hidden" name="action" value="bkf_dd_add_closed" />
+						<p style="margin:5px 0;"><input style="margin-left:0;" type="text" name="date" class="closure-date input-text bkf-form-control" required placeholder="<?php echo $phtext; ?>" autocomplete="off" /></p>
+						<p style="margin:5px 0;"><input type="submit" class="button button-primary button-large" value="<?php esc_html_e('Add Date as Closed', 'bakkbone-florist-companion'); ?>"></p>
+					</form>
+				</div>
+				<div class="bkf-form" style="max-width:200px;text-align:center;margin:0 0 5px 0;">
+					<form id="add-full" action="<?php echo $ajaxurl; ?>" />
+						<h4 style="margin:0;"><?php esc_html_e('Add Fully Booked Date', 'bakkbone-florist-companion'); ?></h4>
+						<?php wp_nonce_field('bkf', 'nonce'); ?>
+						<input type="hidden" name="action" value="bkf_dd_add_full" />
+						<p style="margin:5px 0;"><input style="margin-left:0;" type="text" name="date" class="closure-date input-text bkf-form-control" required placeholder="<?php echo $phtext; ?>" autocomplete="off" /></p>
+						<p style="margin:5px 0;"><input type="submit" class="button button-primary button-large" value="<?php esc_html_e('Add Date as Fully Booked', 'bakkbone-florist-companion'); ?>"></p>
+					</form>
+				</div>
+			</div>
+		<script id="calendarscript">
+			  document.addEventListener('DOMContentLoaded', function() {
+				var calendarEl = document.getElementById('calendar');
+				var calendar = new FullCalendar.Calendar(calendarEl, {
+					eventClick: function(info) {
+						if (info.event.extendedProps.function !== null) {
+							if (confirm('<?php esc_html_e('Remove block for this date?', 'bakkbone-florist-companion'); ?>')){
+								window.location.href = '<?php echo $ajaxurl . '?action=';?>' + info.event.extendedProps.function + '&ts=' + info.event.extendedProps.ts + '<?php echo '&nonce='.$nonce; ?>';
+							}
+						}
+					},
+					initialView: 'dayGridMonth',
+					views: {
+						dayGridMonth: {
+							dayMaxEventRows: 6,
+							dayHeaderFormat: {
+								weekday: 'long'
+							}
+						}
+					},
+					headerToolbar: {
+						start: 'title',
+						center: '',
+						end: 'today prev,next'
+					},
+					buttonText: {
+						today: 'current month'
+					},
+					firstDay: 1,
+					height: '60vh',
+					events:[
+						<?php
+				$closed = get_option('bkf_dd_closed');
+				$full = get_option('bkf_dd_full');
+				$ct = __('Closed','bakkbone-florist-companion');
+				$gt = __('Closed (Global)','bakkbone-florist-companion');
+				$ft = __('Fully Booked','bakkbone-florist-companion');
+				if(null !== $closed){
+					foreach($closed as $ts => $date){
+						$string = wp_date("Y-m-d",$ts);
+						echo '{ title: \''.$ct.'\', start: \'' . $string . '\', className: \'closedbg\', function: \'bkf_dd_remove_closed\', ts: \''.$ts.'\' }, ';
+					}
+				}
+				
+				if(null !== $full){
+					foreach($full as $ts => $date){
+						$string = wp_date("Y-m-d",$ts);
+						echo '{ title: \''.$ft.'\', start: \''.$string.'\', className: \'fullbg\', function: \'bkf_dd_remove_full\', ts: \''.$ts.'\' }, ';
+					}
+				}
+				
+				if(get_option('bkf_dd_setting')['monday'] == false){
+					echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'1\', className: \'closedbg\', function: null }, ';
+				}
+				if(get_option('bkf_dd_setting')['tuesday'] == false){
+					echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'2\', className: \'closedbg\', function: null }, ';
+				}
+				if(get_option('bkf_dd_setting')['wednesday'] == false){
+					echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'3\', className: \'closedbg\', function: null }, ';
+				}
+				if(get_option('bkf_dd_setting')['thursday'] == false){
+					echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'4\', className: \'closedbg\', function: null }, ';
+				}
+				if(get_option('bkf_dd_setting')['friday'] == false){
+					echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'5\', className: \'closedbg\', function: null }, ';
+				}
+				if(get_option('bkf_dd_setting')['saturday'] == false){
+					echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'6\', className: \'closedbg\', function: null }, ';
+				}
+				if(get_option('bkf_dd_setting')['sunday'] == false){
+					echo '{ title: \''.$gt.'\', startRecur: "'.bkf_get_monday().'", daysOfWeek: \'0\', className: \'closedbg\', function: null }, ';
+				}
+				
+				?>
+				]
+				});
+				calendar.render();
+			  });
+			</script>
+			  <div class="bkf-box"><div class="inside bkf-inside" style="margin:0;">
+			  <div id='calendar'></div></div></div>
+		</div></div>
+		<script id="datepicker">
+			jQuery(document).ready(function( $ ) {
+				jQuery(".closure-date").attr( 'readOnly' , 'true' );
+				jQuery(".closure-date").datepicker( {
+					firstDay: 1,
+					minDate: 0,
+					dateFormat: "DD, d MM yy",
+					hideIfNoPrevNext: true,
+					constrainInput: true,
+					beforeShowDay: blockedDates
+				} );
+  			 var closedDatesList = [<?php
+		 		$closeddates = get_option('bkf_dd_closed');
+				if( !empty($closeddates)){
+				 $i = 0;
+				 $len = count($closeddates);
+				 foreach($closeddates as $date){
+					 $ts = strtotime($date);
+					 $jsdate = wp_date('n,j,Y',$ts);
+					 if ($i == $len - 1) {
+					 echo '['.$jsdate.']';	
+			 } else {
+					 echo '['.$jsdate.'],';		 	
+					 }
+					 $i++;
+			 };}; ?>];
+   			 var fullDatesList = [<?php
+		 		$fulldates = get_option('bkf_dd_full');
+				if( !empty($fulldates)){
+				 $i = 0;
+				 $len = count($fulldates);
+				 foreach($fulldates as $date){
+					 $ts = strtotime($date);
+					 $jsdate = wp_date('n,j,Y',$ts);
+					 if ($i == $len - 1) {
+					 echo '['.$jsdate.']';
+				 } else {
+					 echo '['.$jsdate.'],';		 	
+					 }
+					 $i++;
+				 };}; ?>];
+		 
+		 function blockedDates(date) {
+			 var w = date.getDay();
+			 var m = date.getMonth();
+			 var d = date.getDate();
+			 var y = date.getFullYear();
+			 
+			 <?php if(get_option('bkf_dd_setting')['monday'] == false){ ?>
+				if (date >= new Date("<?php echo bkf_get_monday(); ?>") && w == 1) {
+				return [false, "closed", '<?php echo $gt; ?>'];
+				}<?php }; ?>
+				<?php if(get_option('bkf_dd_setting')['tuesday'] == false){ ?>
+				if (date >= new Date("<?php echo bkf_get_monday(); ?>") && w == 2) {
+				return [false, "closed", '<?php echo $gt; ?>'];
+				}<?php }; ?>
+				<?php if(get_option('bkf_dd_setting')['wednesday'] == false){ ?>
+				if (date >= new Date("<?php echo bkf_get_monday(); ?>") && w == 3) {
+				return [false, "closed", '<?php echo $gt; ?>'];
+				}<?php }; ?>
+				<?php if(get_option('bkf_dd_setting')['thursday'] == false){ ?>
+				if (date >= new Date("<?php echo bkf_get_monday(); ?>") && w == 4) {
+				return [false, "closed", '<?php echo $gt; ?>'];
+				}<?php }; ?>
+				<?php if(get_option('bkf_dd_setting')['friday'] == false){ ?>
+				if (date >= new Date("<?php echo bkf_get_monday(); ?>") && w == 5) {
+				return [false, "closed", '<?php echo $gt; ?>'];
+				}<?php }; ?>
+				<?php if(get_option('bkf_dd_setting')['saturday'] == false){ ?>
+				if (date >= new Date("<?php echo bkf_get_monday(); ?>") && w == 6) {
+				return [false, "closed", '<?php echo $gt; ?>'];
+				}<?php }; ?>
+				<?php if(get_option('bkf_dd_setting')['sunday'] == false){ ?>
+				if (date >= new Date("<?php echo bkf_get_monday(); ?>") && w == 0) {
+				return [false, "closed", '<?php echo $gt; ?>'];
+				}<?php }; ?>
+			 
+		 for (i = 0; i < closedDatesList.length; i++) {
+		   if ((m == closedDatesList[i][0] - 1) && (d == closedDatesList[i][1]) && (y == closedDatesList[i][2]))
+		   {
+		   	 return [false,"closed","<?php echo $ct; ?>"];
+		   }
+		 }
+		 for (i = 0; i < fullDatesList.length; i++) {
+		   if ((m == fullDatesList[i][0] - 1) && (d == fullDatesList[i][1]) && (y == fullDatesList[i][2]))
+		   {
+			 return [false,"booked","<?php echo $ft; ?>"];
+		   }
+		 }
+		 return [true];
+	 }
+		 } );
+		</script>
+		<?php
+	}
+	
+	function bkfAddDdBlocksInit(){
+		register_setting(
+			"bkf_dd_blocks_group",
+			"bkf_dd_closed",
+			array('type' => 'array')
+		);
+		register_setting(
+			"bkf_dd_blocks_group",
+			"bkf_dd_full",
+			array('type' => 'array')
+		);
+	}
+		
+}
---
+++ tags/4.0.2/src/dd/timeslots.php
@@ -0,0 +1,347 @@
+<?php
+/**
+ * @author BAKKBONE Australia
+ * @package BKF_Delivery_Date_Timeslots
+ * @license GNU General Public License (GPL) 3.0
+**/
+
+defined("__BKF_EXEC__") or die("Ah, sweet silence.");
+
+class BKF_Delivery_Date_Timeslots{
+
+	function __construct(){
+		global $bkf_dd_ts_db_version;
+		$bkf_dd_ts_db_version = '1.3';
+		register_activation_hook( __FILE__, [$this, 'bkf_dd_ts_db_init']);
+		add_action("admin_menu", [$this, "bkf_admin_menu"], 7);
+		add_action("admin_init", [$this, "bkfAddDdTsInit"]);
+		add_action("plugins_loaded", [$this, "bkf_update_db_check"]);
+	}
+	
+	function bkf_dd_ts_db_init(){
+		global $wpdb;
+		global $bkf_dd_ts_db_version;
+
+		$table_name = $wpdb->prefix . 'bkf_dd_timeslots';
+	
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			method tinytext NOT NULL,
+			day text(9) NOT NULL,
+			start tinytext NOT NULL,
+			end tinytext NOT NULL,
+			fee tinytext,
+			PRIMARY KEY  (id)
+		) $charset_collate;";
+
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		dbDelta( $sql );
+
+		add_option( 'bkf_dd_ts_db_version', $bkf_dd_ts_db_version );
+		
+		$installed_ver = get_option( "bkf_dd_ts_db_version" );
+
+		if ( $installed_ver != $bkf_dd_ts_db_version ) {
+
+			$table_name = $wpdb->prefix . 'bkf_dd_timeslots';
+
+			$sql = "CREATE TABLE $table_name (
+				id mediumint(9) NOT NULL AUTO_INCREMENT,
+				method tinytext NOT NULL,
+				day text(9) NOT NULL,
+				start tinytext NOT NULL,
+				end tinytext NOT NULL,
+				fee tinytext,
+				PRIMARY KEY  (id)
+			) $charset_collate;";
+
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			dbDelta( $sql );
+
+			update_option( 'bkf_dd_ts_db_version', $bkf_dd_ts_db_version );
+		}		
+	}
+	
+	function bkf_update_db_check() {
+		global $bkf_dd_ts_db_version;
+		if ( get_site_option( 'bkf_dd_ts_db_version' ) != $bkf_dd_ts_db_version ) {
+			global $wpdb;
+			global $bkf_dd_ts_db_version;
+
+			$table_name = $wpdb->prefix . 'bkf_dd_timeslots';
+	
+			$charset_collate = $wpdb->get_charset_collate();
+
+			$sql = "CREATE TABLE $table_name (
+				id mediumint(9) NOT NULL AUTO_INCREMENT,
+				method tinytext NOT NULL,
+				day text(9) NOT NULL,
+				start tinytext NOT NULL,
+				end tinytext NOT NULL,
+				fee tinytext,
+				PRIMARY KEY  (id)
+			) $charset_collate;";
+
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta( $sql );
+
+			add_option( 'bkf_dd_ts_db_version', $bkf_dd_ts_db_version );
+		
+			$installed_ver = get_option( "bkf_dd_ts_db_version" );
+
+			if ( $installed_ver != $bkf_dd_ts_db_version ) {
+
+				$table_name = $wpdb->prefix . 'bkf_dd_timeslots';
+
+				$sql = "CREATE TABLE $table_name (
+					id mediumint(9) NOT NULL AUTO_INCREMENT,
+					method tinytext NOT NULL,
+					day text(9) NOT NULL,
+					start tinytext NOT NULL,
+					end tinytext NOT NULL,
+					fee tinytext,
+					PRIMARY KEY  (id)
+				) $charset_collate;";
+
+				require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+				dbDelta( $sql );
+
+				update_option( 'bkf_dd_ts_db_version', $bkf_dd_ts_db_version );
+			}
+		}
+	}
+	
+	function bkf_admin_menu(){
+		$admin_page = add_submenu_page(
+		"bkf_dd",
+		__("Delivery Timeslots","bakkbone-florist-companion"),
+		__("Timeslots","bakkbone-florist-companion"),
+		"manage_woocommerce",
+		"bkf_ddts",
+		[$this, 'bkf_dd_ts_page'],
+		7
+		);
+		add_action( 'load-'.$admin_page, [$this, 'bkf_ts_help_tab'] );
+	}
+	
+	function bkf_ts_help_tab(){
+		$screen = get_current_screen();
+		$id = 'bkf_ts_help';
+		$callback = [$this, 'bkf_ts_help'];
+		$screen->add_help_tab( array( 
+		   'id' => $id,
+		   'title' => __('Documentation','bakkbone-florist-companion'),
+		   'callback' => $callback
+		) );	
+	}
+	
+	function bkf_ts_help(){
+		?>
+		<h2><?php esc_html_e('View documentation for this page at: ','bakkbone-florist-companion'); ?></h2>
+			<a href="https://docs.floristpress.org/dd/timeslots/" target="_blank">https://docs.floristpress.org/dd/timeslots/</a>
+		<?php
+	}
+	
+	function bkf_dd_ts_page(){
+		$nonce = wp_create_nonce("bkf");			
+		$allzones = WC_Data_Store::load('shipping-zone');
+		$rawzones = $allzones->get_zones();
+		$zones = [];
+		$zones[] = new WC_Shipping_Zone( 0 );
+		foreach($rawzones as $rawzone){
+			$zones[] = new WC_Shipping_Zone( $rawzone );
+		}
+		
+		$sm = [];
+		foreach($zones as $zone){
+			$methods = $zone->get_shipping_methods();
+			
+			foreach($methods as $method){
+				$method_is_taxable = $method->is_taxable();
+				$method_is_enabled = $method->is_enabled();
+				$method_instance_id = $method->get_instance_id();
+				$method_title = $method->get_method_title();
+				$method_description = $method->get_method_description();
+				$method_user_title = $method->get_title();
+				$method_rate_id = $method->get_rate_id();
+				$sm[] = array(
+					'enabled'		=>	$method_is_enabled,
+					'taxable'		=>	$method_is_taxable,
+					'instanceid'	=>	$method_instance_id,
+					'title'			=>	$method_title,
+					'description'	=>	$method_description,
+					'usertitle'		=>	$method_user_title,
+					'rateid'		=>	$method_rate_id
+				);
+			}
+		}
+		
+		$days = get_option('bkf_dd_setting');
+		$daysoff = get_option('bkf_dm_setting') !== '' ? get_option('bkf_dm_setting') : [];
+		$ts = bkf_get_timeslots();
+		
+		$tslist = [];
+		
+		foreach($days as $day => $on){
+			if($on){
+				$tslist[$day] = [];
+				foreach($ts as $tslot){
+					if($day == $tslot['day']){
+						$tslist[$day][] = $tslot;
+					}
+				}
+			}
+		}
+		
+		$daymethods = [];
+		foreach($tslist as $day => $slots){
+			$methods = [];
+			foreach($slots as $thisslot){
+				$methods[] = $thisslot['method'];
+			}
+			$daymethods[$day] = $methods;
+		}
+		
+		?>
+		<div class="wrap">
+			<div class="bkf-box">
+			<h1><?php esc_html_e("Delivery Timeslots","bakkbone-florist-companion") ?></h1>
+			<p><?php esc_html_e('Only weekdays enabled on the Delivery Dates page will be displayed below. Any method within a day with no timeslots added will not require timeslots at checkout.','bakkbone-florist-companion') ?><br><em><?php esc_html_e('Fees are optional - leave fee blank if not required for a timeslot.','bakkbone-florist-companion'); ?></em></p>
+			<form class="bkf-form" id="addform" action="<?php echo admin_url('admin-ajax.php') ?>">
+							<input type="hidden" name="action" value="bkf_ts_add" />
+							<?php wp_nonce_field('bkf', 'nonce'); ?>
+								<select id="add-day" name="day" class="bkf-form-control" required>
+									<option value="" disabled selected><?php esc_html_e('Select a day...','bakkbone-florist-companion'); ?></option>
+									<?php
+									foreach($days as $day => $on){
+										if($on == 1){
+											echo '<option value="'.$day.'">'.ucwords($day).'</option>';
+										}
+									}
+										?>
+									</select>
+							<select disabled class="bkf-form-control" name="method" id="add-method" required>
+									<option value="" disabled selected><?php esc_html_e('Select a delivery method...','bakkbone-florist-companion'); ?></option>
+							</select>
+							<label><?php esc_html_e('Start: ', 'bakkbone-florist-companion'); ?><input type="time" class="bkf-form-control" id="add-start" name="start" required /></label>
+							<label><?php esc_html_e('End: ', 'bakkbone-florist-companion'); ?><input type="time" class="bkf-form-control" id="add-end" name="end" required /></label>
+							<label><?php esc_html_e('Fee: ', 'bakkbone-florist-companion'); ?><div class="bkf-input-icon"><input type="text" placeholder="***.**" class="bkf-form-control" id="add-fee" name="fee" pattern="\d+\.\d{2,}" /><i><?php bkf_currency_symbol(true); ?></i></div></label>
+							<input type="submit" value="<?php esc_html_e('Add Timeslot','bakkbone-florist-companion'); ?>" id="add-submit" class="button button-primary" />
+							<div style="max-width:350px;" id="add-error" class="bkf-error bkf-hidden"><p><?php esc_html_e('End time must be greater than start time.','bakkbone-florist-companion'); ?></p></div>
+			</form>
+						<script id="addform">
+							jQuery(function($){
+								const startElement = document.getElementById("add-start");
+								const endElement = document.getElementById("add-end");
+								const dayElement = document.getElementById("add-day");
+								const methodElement = document.getElementById("add-method");
+								const deliveryMethods = [
+									<?php foreach($days as $day => $on){
+										if($on == 1){
+											echo '{ day: "'.$day.'", methods: [';
+												foreach($sm as $smethod){
+													if(array_key_exists($day,$daysoff)){
+														if(!in_array($smethod['rateid'],$daysoff[$day])){
+															echo '{ name: "'.$smethod['title'].' #'.$smethod['instanceid'].': '.$smethod['usertitle'].'", id: "'.$smethod['rateid'].'" },';
+														}
+													} else {
+														echo '{ name: "'.$smethod['title'].' #'.$smethod['instanceid'].': '.$smethod['usertitle'].'", id: "'.$smethod['rateid'].'" },';
+													}
+												}
+											echo '] },';
+										}
+									} ?>];
+								
+								dayElement.addEventListener('change', (event) => {
+									var day = dayElement.value;
+									if( day !== '' ) {
+										methodElement.removeAttribute('disabled');
+					  				  jQuery(methodElement).empty($);
+									  let defaultOption = new Option('<?php esc_html_e('Select a delivery method...', 'bakkbone-florist-companion'); ?>', '');
+									  methodElement.add(defaultOption, 0);
+									  methodElement.options[0].disabled = true;
+										deliveryMethods.forEach(checkAvail);
+										function checkAvail(method){
+											if(method.day == day){
+												method.methods.forEach(doAvail);
+												function doAvail(thismethod){
+													let newOption = new Option(thismethod.name, thismethod.id)
+													methodElement.add(newOption, undefined);
+												}
+											}
+										}
+									}
+								});
+								
+								startElement.addEventListener('change', (event) => {
+									var start = startElement.value;
+									var end = endElement.value;
+									if( start !== '' && end !== '' && start >= end ) {
+										jQuery("#addform").addClass( 'bkf-invalid' );
+										jQuery("#addform").removeClass( 'bkf-validated' );
+										jQuery("#add-error").removeClass( 'bkf-hidden' );
+									} else if(start == '' || end == '') {
+										jQuery("#addform").removeClass( 'bkf-invalid' );
+										jQuery("#addform").removeClass( 'bkf-validated' );				  
+										jQuery("#add-error").addClass( 'bkf-hidden' );
+								  } else {
+										jQuery("#addform").addClass( 'bkf-validated' );
+										jQuery("#add-error").addClass( 'bkf-hidden' );
+									}
+								});
+								
+								endElement.addEventListener('change', (event) => {
+									var start = startElement.value;
+									var end = endElement.value;
+									if( start !== '' && end !== '' && start >= end ) {
+										jQuery("#addform").addClass( 'bkf-invalid' );
+										jQuery("#addform").removeClass( 'bkf-validated' );
+										jQuery("#add-error").removeClass( 'bkf-hidden' );
+									} else if(start == '' || end == '') {
+										jQuery("#addform").removeClass( 'bkf-invalid' );
+										jQuery("#addform").removeClass( 'bkf-validated' );				  
+										jQuery("#add-error").addClass( 'bkf-hidden' );
+								  } else {
+										jQuery("#addform").addClass( 'bkf-validated' );
+										jQuery("#add-error").addClass( 'bkf-hidden' );
+									}
+								});
+							});
+							</script>
+			<div style="columns: 500px 3;width:100%;">
+				<?php
+				foreach($days as $day => $on){
+					if($on == 1){?><div class="inside bkf-inside" style="break-inside:avoid;margin-top:0;margin-left:0;margin-right:0;">
+						<h2 style="margin-top:0;text-align:center;"><?php echo ucwords($day); ?></h2>
+						<?php foreach($sm as $smethod){
+							if(in_array($smethod['rateid'], $daymethods[$day])){
+								echo '<div class="bkf-form" id="'.$day.'-'.$smethod['instanceid'].'"><p style="margin:0;"><strong>'.$smethod['title'].' #'.$smethod['instanceid'].': </strong>'.$smethod['usertitle'].'</p>';
+								foreach($ts as $tslot){
+									if($tslot['day'] == $day && $tslot['method'] == $smethod['rateid']){
+											echo '<p>'.date("g:i a", strtotime($tslot['start'])).' - '.date("g:i a", strtotime($tslot['end']));
+											if($tslot['fee'] !== '' && $tslot['fee'] !== null){
+												echo ', '.bkf_currency_symbol().$tslot['fee'];
+											}
+											echo ' <em><a href="'.admin_url('admin-ajax.php?action=bkf_ts_del&nonce='.$nonce.'&id='.$tslot['id']).'">'.esc_html__('Delete','bakkbone-florist-companion').'</a></em></p>';
+										}
+									}
+									?></div><?php
+								}
+							}?>
+						</div><?php
+					}
+		}
+		?></div></div></div><?php
+	}
+	
+	function bkfAddDdTsInit(){
+		register_setting(
+			"bkf_dd_ts_group",
+			"bkf_dd_timeslots",
+			array('type' => 'array')
+		);
+	}
+	
+}
